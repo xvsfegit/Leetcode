f5d08ddc96a97706ce30b95e4d20a2067d09a172
xvsfegit <xvsfegit@126.com>
2014-12-03 14:49:29 +0800

    complete goto_table and debug info

--- ac_search.cpp
+++ ac_search.cpp
@@ -4,18 +4,22 @@
 	> Mail:xvsfekcn@gmail.com 
 	> Created Time: 二 12/ 2 21:26:41 2014
  ************************************************************************/
-
 #include<iostream>
 #include<string>
 #include<string.h>
 #include<stdlib.h>
 #include<vector>
-const int PATTERN_NUM = 5;
-const int MAX_STATE = 100;
+#include<set>
+#include<memory.h>
+
+using namespace std;
+const int PATTERN_NUM = 4;
+const int MAX_STATE = 20;
 const int ALP = 26; 
 int _GOTO_TABLE[MAX_STATE][ALP];
 int _FAIL_TABLE[MAX_STATE];
-using namespace std;
+std::set<string> _out[MAX_STATE];
+
 
 void read_pattern(vector<string>& pattern_vec)
 {
@@ -26,32 +30,57 @@ void read_pattern(vector<string>& pattern_vec)
         pattern_vec.push_back(tmp);
     }   
 }
+void build_fail_table(const vector<string>&)
+{
+        
+}
 
-int build_goto_table(const vector<string>& pattern_vec)
+void build_goto_table(const vector<string>& pattern_vec)
 {
     const char a = 'a';
     int used_state = 0;
-    int t = 0;
-
     for(vector<string>::const_iterator piter = pattern_vec.begin();piter != pattern_vec.end(); ++piter){
-        for(int i = 0;i != (*piter).size(); ++i){
-            if(_GOTO_TABLE[t][piter[i] - a] == 0){
-                _GOTO_TABLE[t][piter[i] - a] == ++used_state;
+        string::const_iterator sit = piter->begin();
+        int t = 0;
+        for(;sit != piter->end(); ++sit){
+            if(_GOTO_TABLE[t][(*sit) - a] == 0){
+                _GOTO_TABLE[t][(*sit) - a] = ++used_state;
                 t = used_state;
             }   
             else{
-                t = _GOTO_TABLE[t][piter[i] - a];    
+                t = _GOTO_TABLE[t][(*sit) - a];    
             }
         }
+        _out[t].insert(*piter);
     }
+#ifdef DEBUG
+    for(int i = 0;i < MAX_STATE; ++i){
+        for(int j = 0;j < ALP; ++j){
+            cout << _GOTO_TABLE[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for(int i = 0;i != MAX_STATE; ++i){
+        int len = _out[i].size();
+        if(len != 0){
+            set<string>::const_iterator iter = _out[i].begin();
+            cout << i << ":";
+            for(;iter != _out[i].end(); ++iter){
+                cout << *iter << " ";
+            }
+            cout << endl;
+        }
+    }
+#endif
     
 }
 
 int main(int argc,char* argv[])
 {
+    memset(_GOTO_TABLE,0,MAX_STATE * ALP * 4);
     vector<string> pattern_vec;
     read_pattern(pattern_vec);
-    cout << pattern_vec.size() << endl;
+    build_goto_table(pattern_vec);
     return 0;
 }
 
