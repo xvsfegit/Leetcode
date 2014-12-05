26b44d7b94142dcf2d4f71b81891326bba6d3980
xvsfegit <xvsfegit@126.com>
2014-12-05 16:29:21 +0800

    init trie

---
+++ trie.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: trie.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com
+	> Created Time: 2014年12月05日 星期五 16时29分03秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(int argc,char* argv[])
+{
+    return 0;
+}
