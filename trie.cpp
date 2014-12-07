3de100b38822db54b1c2bf2bec6c3e43d65f7929
zhaozheng <zhaozheng@zhaozhengdeMBP.lan>
2014-12-07 22:47:44 +0800

    for build trie tree

--- trie.cpp
+++ trie.cpp
@@ -11,12 +11,11 @@
 
 using namespace std;
 const int MAX = 26;
+const char a = 'a';
 struct trie_node{
     bool is_str;
+    string current_string;
     trie_node* next[MAX];
-};
-struct trie{
-    trie_node* root;
 };
 
 int read_pattern(vector<string>& pattern_vec)
@@ -27,14 +26,39 @@ int read_pattern(vector<string>& pattern_vec)
         cin >> tmp;
         if(tmp == "eof"){
             break;
-        } 
-        pattern_vec.push_back(tmp);
+        } mZ        pattern_vec.push_back(tmp);
     }
     return pattern_vec.size();
+}
+trie_node* create_trie(const vector<string>& pattern_vec)
+{
+    trie_node* root = new trie_node;
+    for(vector<string>::const_iterator piter = pattern_vec.begin();\
+        piter != pattern_vec.end(); ++piter){
+        trie_node* current_node = root;
+        for(string::const_iterator siter = piter->begin() \
+            ;siter != piter->end(); ++siter){
+            unsigned int pos = *siter - a;
+            if(current_node->next[pos] == NULL){
+                current_node->next[pos] = new trie_node;     
+            }
+            current_node = current_node->next[pos];
+        }     
+        current_node->is_str = true;
+        current_node->current_string = *piter;
+    }
+#ifdef DEBUG
+
+
+#endif
+    return root;
 }
 
 
 int main(int argc,char* argv[])
 {
+    vector<string> pattern_vec;
+    read_pattern(pattern_vec);
+    trie_node* root = create_trie(pattern_vec);
     return 0;
 }
