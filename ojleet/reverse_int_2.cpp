bf6514952651d0265b82ace3c5a43f7ce88d0388
xvsfegit <xvsfegit@126.com>
2014-12-15 20:18:14 +0800

    fast

--- ojleet/reverse_int_2.cpp
+++ ojleet/reverse_int_2.cpp
@@ -12,19 +12,15 @@ using namespace std;
 int reverse(int x){
     const int MAX_INT = 2147483647;
     const int MIN_INT = -2147483648;
-
-    if(x == 0){
-        return x;
-    }
     int n = 0;
-    int y = 0;
+    long long y = 0;
     while(x != 0){
         n = x % 10;
-        if(y > MAX_INT/10 || y < MIN_INT/10){
-            return 0;
-        }
         y = y * 10 + n;
         x = x / 10;
+    }
+    if(y > MAX_INT || y < MIN_INT){
+        return 0;
     }
     return y;
 }
@@ -33,7 +29,6 @@ int main(int argc,char* argv[])
     cout << "Please input the int:";
     int x = 0;
     cin >> x;
-    cout << x << endl;
     cout << reverse(x) << endl;
     return 0;
 }
