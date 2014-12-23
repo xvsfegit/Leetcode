a1b6adbb96353b3a2f3990ddcde23f9e62592c39
xvsfegit <xvsfegit@126.com>
2014-12-24 00:02:29 +0800

    test

---
+++ ojleet/addBinary.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: addBinary.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com
+	> Created Time: 2014年12月23日 星期二 23时26分17秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+string addBinary(string a,string b)
+{
+    int alen = a.size();
+    int blen = b.size();
+    int tmp = 0;
+    int flag = 0;
+    for(int i = 0;i < alen; ++i){
+        tmp = a[i] - '0';
+        if()
+    }
+    
+}
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
