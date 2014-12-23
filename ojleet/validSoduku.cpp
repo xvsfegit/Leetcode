62386c871f7c3979c2db39090d9b4132a6ad58c8
xvsfegit <xvsfegit@126.com>
2014-12-23 23:13:33 +0800

    not complete

---
+++ ojleet/validSoduku.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+	> File Name: validSoduku.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 二 12/23 20:49:24 2014
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<string.h>
+using namespace std;
+#define SODU 9
+
+const char zero = '0';
+
+bool isValidSudoku(vector< vector<char> >& board)
+{
+    int w_flag[SODU];
+    int h_flag[SODU][SODU];
+    for(int i = 0;i < SODU; ++i){
+        for(int j = 0;j < SODU; ++j){
+            if(board[i][j] == '.')
+                continue;
+            int w = board[i][j] - zero - 1;
+            if(w_flag[w] == 1 || h_flag[j][w] == 1){
+                return false;
+            }
+            w_flag[w] = 1;
+            h_flag[j][w] = 1;
+            
+        }
+    }
+    return true;
+}
+
+int main(int argc,char* argv[])
+{
+    
+    return 0;
+}
