d96f119b54053eff1d08fbb5a86a23d3fd9189bc
xvsfegit <xvsfegit@126.com>
2015-01-04 23:37:30 +0800

    dp first test

---
+++ ojleet/climbingStairs.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: climbingStairs.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 日  1/ 4 22:07:05 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int climbStairs(int n)
+{
+    if(n == 0){
+        return 0;
+    }
+    return n;
+}
+
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
