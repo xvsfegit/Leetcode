cc2339093c5a90b1b18c1fe40ea40aab49e67d06
xvsfegit <xvsfegit@126.com>
2015-01-18 23:58:13 +0800

    build CompareVersion file

---
+++ ojleet/CompareVersionNumbers.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: CompareVersionNumbers.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com
+	> Created Time: 2015年01月18日 星期日 23时57分06秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int compareVersion(string version1,string version2)
+{
+    return -1;
+}
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
