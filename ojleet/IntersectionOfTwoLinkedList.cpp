cef6cc9a6cb7af7dddbc2013bc805114212c7688
xvsfegit <xvsfegit@126.com>
2015-01-20 00:31:11 +0800

    build file

---
+++ ojleet/IntersectionOfTwoLinkedList.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: IntersectionOfTwoLinkedList.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com
+	> Created Time: 2015年01月20日 星期二 00时20分03秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+};
+
+ListNode* getIntersectionNode(ListNode* headA,ListNode* headB)
+{
+    return NULL;
+}
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
