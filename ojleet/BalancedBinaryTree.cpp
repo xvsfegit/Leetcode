da61dd4eaf66bd503d7ae30bae1065fdb004996e
xvsfegit <xvsfegit@126.com>
2015-01-22 23:47:00 +0800

    so slowly code

--- ojleet/BalancedBinaryTree.cpp
+++ ojleet/BalancedBinaryTree.cpp
@@ -7,6 +7,7 @@
 
 #include<iostream>
 #include<queue>
+#include<math.h>
 using namespace std;
 struct TreeNode{
     int val;
@@ -16,39 +17,39 @@ struct TreeNode{
 };
 /*
  */
+int treeHight(TreeNode* root)
+{
+    if(root == NULL){
+        return 0;
+    }
+    int left = treeHight(root->left) + 1;
+    int right = treeHight(root->right) + 1;
+    return right > left ? right : left;
+}
 bool isBalanced(TreeNode* root)
 {
     if(root == NULL)
         return true;
-    queue<TreeNode*> remainNodeQ;
-    remainNodeQ.push(root);
-    while(!remainNodeQ.empty()){
-        int remianLen = remainNodeQ.size();
-        while(remianLen--){
-            TreeNode* curNode = remainNodeQ.front();
-            remainNodeQ.pop();
-            if(curNode->left){
-                remainNodeQ.push(curNode->left);
-            }
-            if(curNode->right){
-                remainNodeQ.push(curNode->right);
-            }
-        }
-    }
-    return true;
+    bool leftResult = isBalanced(root->left);
+    bool rightResult = isBalanced(root->right);
+    int leftHight = treeHight(root->left);
+    int rightHight = treeHight(root->right);
+    bool comResult = abs(leftHight - rightHight) <= 1;
+    return leftResult && rightResult && comResult; 
 }
 
 int main(int argc,char* argv[])
 {
     TreeNode* p = new TreeNode(0);
-    p->left = new TreeNode(1);
+    //p->left = new TreeNode(1);
     p->right = new TreeNode(2);
-    p->right->left = new TreeNode(5);
-    p->right->left->right = new TreeNode(5);
+    //p->right->left = new TreeNode(5);
+    //p->right->left->right = new TreeNode(5);
     p->right->right = new TreeNode(6);
-    p->right->right->right = new TreeNode(6);
-    p->left->left = new TreeNode(3);
-    p->left->left->left = new TreeNode(4);
-    cout << maxDepth(p) << endl;
+    //p->right->right->right = new TreeNode(6);
+    //p->left->left = new TreeNode(3);
+    //p->left->left->left = new TreeNode(4);
+    cout << treeHight(p) << endl;
+    cout << isBalanced(p) << endl;
     return 0;
 }
