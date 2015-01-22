d1b7cf36a09f37516b55230e71b8a13891ae522b
xvsfegit <xvsfegit@126.com>
2015-01-23 00:12:33 +0800

    a begin about code

---
+++ ojleet/PascalTriangle.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: PascalTriangle.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 四  1/22 23:55:19 2015
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+vector<vector<int> > generateTriangle(int numRows)
+{
+    vector<vector<int> > result;
+    for(int i = 0;i < numRows; ++i){
+        vector<int> tmpVec;
+        for(int j = 0;j < i; ++j){
+            
+        }
+    return result;
+}
+
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
