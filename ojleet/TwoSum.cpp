2cb09a84541a54dd7fb818e763a2997d174a3cc9
xvsfegit <xvsfegit@126.com>
2015-01-24 01:23:24 +0800

    initialize file

---
+++ ojleet/TwoSum.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: TwoSum.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com
+	> Created Time: 2015年01月24日 星期六 01时20分34秒
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& numbers,int target)
+{
+    vector<int> result;
+    return result;
+}
+
+
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
