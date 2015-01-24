5768c49fe7b1b88cdb74fd7613d69c77260f7ce1
xvsfegit <xvsfegit@126.com>
2015-01-24 18:24:10 +0800

    Init code file

---
+++ ojleet/AddTwoNumbers.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: AddTwoNumbers.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com
+	> Created Time: 2015年01月24日 星期六 18时23分51秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(int argc,char* argv[])
+{
+    return 0;
+}
