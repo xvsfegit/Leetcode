8defe65384b6d1c3b94d4cb728907314281b1766
xvsfegit <xvsfegit@126.com>
2015-01-26 00:11:59 +0800

    first test

--- ojleet/PascalTriangle.cpp
+++ ojleet/PascalTriangle.cpp
@@ -13,16 +13,48 @@ using namespace std;
 vector<vector<int> > generateTriangle(int numRows)
 {
     vector<vector<int> > result;
-    for(int i = 0;i < numRows; ++i){
+    if(numRows == 0){
+        return result;
+    }
+    vector<int> beginVec(1,1);
+    result.push_back(beginVec);
+    if(numRows >= 2){
+        vector<int> secVec(2,1);
+        result.push_back(secVec);
+    }
+    for(int i = 2;i < numRows; ++i){
         vector<int> tmpVec;
-        for(int j = 0;j < i; ++j){
-            
+        tmpVec.push_back(1);
+        int j = 1;
+        for(;j < (i + 1)/ 2; ++j){
+            int tmpValue = result[i - 1][j - 1] + result[i - 1][j]; 
+            tmpVec.push_back(tmpValue);
         }
+        int k = 0;
+        if(i % 2){
+            k = j - 1; 
+        }
+        else{
+            k = j - 2;
+        }
+        for(;j < i + 1; ++j,--k){
+            tmpVec.push_back(tmpVec[k]);
+        }
+        result.push_back(tmpVec);
+    }
     return result;
 }
 
 
 int main(int argc,char* argv[])
 {
+    vector<vector<int> > result;
+    result = generateTriangle(4);
+    for(int i = 0;i < result.size(); ++i){
+        for(int j = 0;j < result[i].size(); ++j){
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
