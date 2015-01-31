a58e427c5e2563c7cdf4db748b10faca38ca24b5
xvsfegit <xvsfegit@126.com>
2015-01-31 22:44:32 +0800

    complete a frame

--- ojleet/CompareVersionNumbers.cpp
+++ ojleet/CompareVersionNumbers.cpp
@@ -6,11 +6,38 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
 using namespace std;
 
 
 int compareVersion(string version1,string version2)
 {
+    int flag = 0;
+    for(int i = 0,j = 0;i < version1.size() && j < version2.size();){
+        if(version1[i] != '.'){
+            ++i;
+        }
+        else{
+            flag = 1;
+        }
+        if(version2[j] != '.'){
+            ++j;
+        }
+        else{
+            flag += 1;
+        }
+        if(flag == 2){
+            if(i == j){
+
+            }
+            else if (i < j){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+        }
+    }
     return -1;
 }
 
