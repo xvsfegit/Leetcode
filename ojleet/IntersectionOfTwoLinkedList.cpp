fe38b015889123b4b1c1dd4efec32d07bcecc9d6
xvsfegit <xvsfegit@126.com>
2015-02-02 00:02:00 +0800

    reverse is not ok

--- ojleet/IntersectionOfTwoLinkedList.cpp
+++ ojleet/IntersectionOfTwoLinkedList.cpp
@@ -10,14 +10,53 @@ using namespace std;
 struct ListNode{
     int val;
     ListNode* next;
+    ListNode(int x):val(x),next(NULL){}
 };
+
+ListNode* reverseList(ListNode* head)
+{
+    ListNode* p = head;
+    ListNode* q = p->next;
+    ListNode* r;
+    while(q){
+        r = q->next;
+        q->next = p;
+        p = q;
+        q = r;
+    }
+    head->next = NULL;
+    return p;    
+}
 
 ListNode* getIntersectionNode(ListNode* headA,ListNode* headB)
 {
-    return NULL;
+    if(headA == NULL || headB == NULL){
+        return NULL;
+    }
+    headA = reverseList(headA);
+    headB = reverseList(headB);
+    ListNode* result = NULL;
+    while(headA && headB){
+        if(headA == headB){
+            headA = headA->next;
+            headB = headB->next;
+            result = headA;
+        }
+    }
+    return result;
 }
 
 int main(int argc,char* argv[])
 {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(3);
+    head = reverseList(head);
+    while(head){
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
     return 0;
 }
