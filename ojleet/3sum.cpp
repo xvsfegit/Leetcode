cef943d5bf177f1140f39dc2201285e5e91fb585
xvsfegit <xvsfegit@126.com>
2015-03-03 22:17:11 +0800

    init it

---
+++ ojleet/3sum.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: 3sum.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 二  3/ 3 22:14:50 2015
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution{
+public:
+    vector<vector<int> > threeSum(vector<int>& num)
+    {
+        vector<vector<int> > result;
+        return result;
+    }
+};
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
