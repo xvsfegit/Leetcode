87eb240ab0ecfff2e003c08f5a202d57444877bb
xvsfegit <xvsfegit@126.com>
2015-05-01 13:06:09 +0800

    for number 17

---
+++ ojleet/LetterCombinationOfaPhoneNumber.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: LetterCombinationOfaPhoneNumber.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 五  5/ 1 13:00:52 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
