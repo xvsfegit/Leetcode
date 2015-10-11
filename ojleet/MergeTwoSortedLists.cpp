82b54565ed82ef6c40134558520275d1c0c5ed4c
xvsfegit <xvsfegit@126.com>
2015-10-11 23:22:52 +0800

    make a new file

---
+++ ojleet/MergeTwoSortedLists.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: MergeTwoSortedLists.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 日 10/11 23:18:54 2015
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x):val(x),next(NULL){}
+};
+
+class Solution{
+public:
+    ListNode* mergeTwoLists(ListNode* l1,ListNode* l2)
+    {
+        ListNode* result;
+        return result;
+    }
+};
