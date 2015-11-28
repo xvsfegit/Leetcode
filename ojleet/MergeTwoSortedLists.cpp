fa19d59f86b7d734c2c4655905292aa654a59bf5
xvsfegit <xvsfegit@126.com>
2015-11-29 00:15:03 +0800

    for malloc a new pointer

--- ojleet/MergeTwoSortedLists.cpp
+++ ojleet/MergeTwoSortedLists.cpp
@@ -24,6 +24,11 @@ public:
         if(l2 == NULL){
             return l1;
         }
+        if(l2->val < l1->val){
+            ListNode* tmp = l1;
+            l1 = l2;
+            l2 = tmp;
+        }
         ListNode* p = l1;
         ListNode* q = l1;
         while(l1 && l2){
@@ -43,6 +48,32 @@ public:
         }
         return p;
         
+    }
+    
+    ListNode* mergeTwoLists_2(ListNode* l1,ListNode* l2)
+    {
+        ListNode* head = new ListNode(-1);
+        ListNode* p = head;
+        while(l1!=NULL || l2!= NULL)
+        {
+            int val1 = l1==NULL?INT_MAX:l1->val;
+            int val2 = l2==NULL? INT_MAX:l2->val;
+            if(val1<=val2)
+            {
+                p->next = l1;
+                l1=l1->next;
+            }
+            else
+            {
+                p->next = l2;
+                l2 = l2->next;
+                
+            }
+            p= p->next;
+        }
+        p = head->next;
+        delete head;
+        return p;
     }
 };
 int main(int argc,char* argv)
