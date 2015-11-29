2b2b99abe23bd01c4ad4b7930eb09b1cce61981f
xvsfegit <xvsfegit@126.com>
2015-11-29 10:36:57 +0800

    init code

---
+++ ojleet/GenerateParentheses.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: GenerateParentheses.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 日 11/29 09:59:18 2015
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution{
+public:
+    vector<string> generateParenthesis(int n)
+    {
+        vector<string> result;
+        if(n == 0){
+            return result;
+        }
+
+        return result;
+    }
+
+};
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
