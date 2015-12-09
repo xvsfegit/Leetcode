c4c5046e9c7834944bfb317e933b519bda9d7157
xvsfegit <xvsfegit@126.com>
2015-12-10 01:06:40 +0800

    temp code for alg

--- ojleet/GenerateParentheses.cpp
+++ ojleet/GenerateParentheses.cpp
@@ -19,13 +19,30 @@ public:
         if(n == 0){
             return result;
         }
-
+        string tempString;
+        generateDeep(n,n,tempString,result);
+        for(int i;i < result.size(); ++i){
+            std::cout << result[i] << std::endl;
+        }
         return result;
     }
-
+    void generateDeep(int leftNum,int rightNum,string& s,vector<string>& result)
+    {
+        if(leftNum == 0 && rightNum == 0){
+            result.push_back(s);
+        }        
+        if(leftNum > 0){
+            generateDeep(leftNum - 1,rightNum,s.append("("),result);
+        }
+        if(rightNum > 0 && leftNum < rightNum){
+            generateDeep(leftNum,rightNum - 1,s.append(")"),result);
+        }
+    }
 };
 
 int main(int argc,char* argv[])
 {
+    Solution testClass;
+    testClass.generateParenthesis(2);
     return 0;
 }
