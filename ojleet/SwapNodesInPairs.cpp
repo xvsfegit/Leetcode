f431f029a497b5ef910da3b182ae3ec0700a2357
xvsfegit <xvsfegit@126.com>
2015-12-10 22:38:37 +0800

    add new problem about swap nodes

---
+++ ojleet/SwapNodesInPairs.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: SwapNodesInPairs.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 四 12/10 22:35:35 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x):val(x),next(NULL){}
+};
+
+class Solution{
+public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        return NULL;
+    }
+
+};
+
+
+int main(int argc,char* argcv[])
+{
+
+}
