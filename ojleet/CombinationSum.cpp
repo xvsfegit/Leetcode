4bf1d77f73cb1ab9223ebe1f181794a682e01c8d
xvsfegit <xvsfegit@126.com>
2016-03-21 22:58:20 +0800

    init comsum

---
+++ ojleet/CombinationSum.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: CombinationSum.cpp
+	> Author:Kcn
+	> Mail:xvsfekcn@gmail.com 
+	> Created Time: 一  3/21 22:57:25 2016
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+
+};
+
+int main(int argc,char* argv[]){
+    return 0;
+}
