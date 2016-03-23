403fa9fa20c304575c514bbc175ef7d2996c18d0
xvsfegit <xvsfegit@126.com>
2016-03-24 00:41:05 +0800

    completed

--- ojleet/CombinationSum.cpp
+++ ojleet/CombinationSum.cpp
@@ -6,10 +6,51 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 class Solution{
+private:
+    vector< vector<int> > results;
+    const int index_count;
+    int* index;
+    int target;
 public:
+    Solution():index_count(10000)
+    {
+        index = new int[index_count];
+    };
+    ~Solution()
+    {
+        delete [] index;
+    }
+
+    void dfs(vector<int>& candidates,int sum,int n){
+        if(sum > target){
+            return;
+        }
+        else if(sum == target){
+            vector<int> tmpVec;
+            for(int i = 1;i <= n; ++i){
+                tmpVec.push_back(candidates[index[i]]);
+            }
+            results.push_back(tmpVec);
+        }
+        for(int i = index[n];i < candidates.size(); ++i){
+            index[n + 1] = i;
+            dfs(candidates,sum + candidates[i],n + 1);
+        }
+    }
+
+    vector< vector<int> > combinationSum(vector<int>& candidates,int target){
+        sort(candidates.begin(),candidates.end());    
+        this->target = target;
+        results.clear();
+        memset(index,0,sizeof(int) * index_count);
+        dfs(candidates,0,0);
+        return results;
+    }
 
 };
 
